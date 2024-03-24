6ac462c37b6e5bbacfa63c3561b5a01f40d05862
Anisha Iyer <anishaiyer@berkeley.edu>
2024-03-24 03:29:56 -0700

    minor changes to class architecture

--- well.py
+++ well.py
@@ -2,17 +2,31 @@
     Well class to enable modular framework for Ca2+ imaging analysis. One Well object represents the data contained in one file of data
     for one well imaged on one day (within a folder for all wells imaged that day)
 
-    Parents: File / Load class if modularized (load.py)
-    Children: Cell (cell.py)
+    Upper: File / Load class if modularized (load.py)
+    Middle: Well
+    Lower: Cell
 """
 
 class Well():
     def __init__(filename, n_perfs):
-        filename = filename
-        n_perfs = n_perfs
-        perfusions = {}
+        self.filename = filename
+        self.n_perfs = n_perfs
+        self.perfusions = {}
+        self.cells = []
+
+    def add_perfusion(compound, cycle_num):
+        if str.lower(compound) not in self.perfusions.keys():
+            self.perfusions[compound] = cycle_num
+        self.perfusions[compound].extend(cycle_num)
+
+class Cell():
+    def __init__(ID, loc, size, A, well):
+        self.ID = ID
+        self.location = loc
+        self.size = size
+        self.area = A
+        self.well = well
+        self.F = []
     
-    def add_perfusion(compound, cycle):
-        perfusions[compound] = cycle
-    
-    
+    def set_fluoros():
+        pass
